fea7bfff9639e086903c7fb2c34749ab16bbed53
yanjifa <yanjifa2013@qq.com>
2024-10-14 20:39:07 +0800

    :art: save

--- scripts/build-ios.groovy
+++ scripts/build-ios.groovy
@@ -1,28 +1,22 @@
 def build(String platform, Map params) {
     stage("Build ${platform}") {
         echo "Building ${platform}..."
-        steps {
-            def workspaceDir = "workspace-${platform}"
-            dir("${workspaceDir}") {
-                // 准备工作区
-                script {
-                    def script = load 'utils.groovy'
-                    script.makeWorkSpace()
-                }
-                // 构建 Cocos 项目, 重试 3 次
-                retry(3) {
-                    script {
-                        try {
-                            def utils = load 'utils.groovy'
-                            def buildArg = "platform=${platform};buildPath=./build;template=default;debug=false;"
-                            utils.buildCocos(buildArg)
-                        } catch (Exception e) {
-                            // 如果检出失败，等待120秒后再次尝试s
-                            echo "Build Cocos failed, retrying in 120 seconds. Error: ${e.getMessage()}"
-                            sleep time: 120, unit: 'SECONDS'
-                            throw e // 重新抛出异常以确保可以被 retry 捕获
-                        }
-                    }
+        def workspaceDir = "workspace-${platform}"
+        dir("${workspaceDir}") {
+            // 加载工具类
+            def utils = load 'utils.groovy'
+            // 准备工作区
+            utils.makeWorkSpace()
+            // 构建 Cocos 项目, 重试 3 次
+            retry(3) {
+                try {
+                    def buildArg = "platform=${platform};buildPath=./build;template=default;debug=false;"
+                    utils.buildCocos(buildArg)
+                } catch (Exception e) {
+                    // 如果检出失败，等待120秒后再次尝试s
+                    echo "Build Cocos failed, retrying in 120 seconds. Error: ${e.getMessage()}"
+                    sleep time: 120, unit: 'SECONDS'
+                    throw e // 重新抛出异常以确保可以被 retry 捕获
                 }
             }
         }
