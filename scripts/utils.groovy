9a1f00cb01a2bb04035c81dcf564778faf97d29a
yanjifa <yanjifa2013@qq.com>
2024-10-15 15:34:08 +0800

    :art: save

--- scripts/utils.groovy
+++ scripts/utils.groovy
@@ -19,24 +19,12 @@ def makeWorkSpace() {
     // 使用 Jenkins 的 WORKSPACE 环境变量
     def workspaceDir = "${env.WORKSPACE}/bingo-frenzy-client"
 
-    // 删除当前目录中不在 items 列表中的文件和目录
-    new File('.').eachFile { file ->
-        if (!items.contains(file.name) || !file.isSymlink()) {
-            // 如果文件或目录不在列表中或不是符号链接，则删除
-            if (file.isDirectory()) {
-                file.deleteDir()
-            } else {
-                file.delete()
-            }
-        }
-    }
+    // 清空当前目录
+    sh "rm -rf *"
 
     // 对每一个项目元素创建符号链接
     items.each { item ->
-        def target = new File(item)
-        if (!target.exists()) {
-            sh "ln -s ${workspaceDir}/${item} ${item}"
-        }
+        sh "ln -s ${workspaceDir}/${item} ${item}"
     }
 }
 
